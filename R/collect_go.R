01d8d5bfc6e55b046f1a82a4bde3def816ce064b
zhaoqi.zhang <zhaoqi.zhang@shopee.com>
2024-04-18 23:50:51 +0800

    finish R code

---
+++ R/collect_go.R
@@ -0,0 +1,70 @@
+#### intalling and loading libraries
+library(BiocParallel)
+register(MulticoreParam(8))
+
+library(dplyr)
+library(data.table)
+
+library(Biobase)
+library(org.Hs.eg.db)
+library(OrganismDbi)
+
+
+#### reading count matrix
+library(data.table)
+counts = fread("GSE124326_count_matrix.txt.gz")
+colnames(counts) <- sapply(colnames(counts), function(x){sub(".counts","",x)})
+counts[,1:3] %>% head
+
+### reading phenotypic data
+pheno = fread("GSE124326_pheno.txt")
+pheno %>% head
+
+#### Checking and mapping counts and phenotypic data
+phenoNames = pheno$sample
+pheno1 = pheno[,-c("sample")]
+rownames(pheno1) <- phenoNames
+
+countsNames <- sub("\\..*", "", counts$gene) # remove version number
+counts1 = counts[,-c("gene")]
+rownames(counts1) <- countsNames
+
+#Remove the samples in counts that are missing in pheno 
+missingNames <- setdiff(colnames(counts1), rownames(pheno1)) # elements of x not in y
+
+counts1 <- counts1[, !c(..missingNames)]
+rownames(counts1) <- countsNames
+
+#check samples of counts match with those phenotypic information
+all(colnames(counts1) == rownames(pheno1))
+
+# convert to data frame
+pheno1 <- as.data.frame(pheno1)
+rownames(pheno1) <- phenoNames
+counts1 <- as.data.frame(counts1)
+rownames(counts1) <- countsNames
+pheno1 %>% head
+
+# Remove genes with low counts
+keep <- (rowSums(counts1) >= 10)
+counts1 <- counts1[keep,]
+dim(counts1)
+
+# combine BP1 and BP2 as BD
+pheno1 <- pheno1 %>%
+  mutate(condition = factor(ifelse(diagnosis %in% c("BP1", "BP2"), "BD", "Control"), levels = c("Control", "BD")))
+rownames(pheno1) <- phenoNames
+pheno1 %>% head
+counts1 %>% head
+
+# get the GO terms for the genes 
+ensemble_keys <- unique(rownames(counts1))
+tnf <- OrganismDbi::select(org.Hs.eg.db, keys=ensemble_keys, keytype="ENSEMBL", columns=c("ENTREZID", "GO"))
+tnf <- tnf[tnf$ONTOLOGY == "BP",]
+tnf %>% head
+
+# save to csv
+write.csv(tnf, "py/GO_BP.csv", row.names = FALSE)
+write.csv(t(counts1), "py/counts1.csv", row.names = TRUE)
+write.csv(pheno1, "py/pheno1.csv", row.names = TRUE)
+
